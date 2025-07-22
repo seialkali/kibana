ca74f09b09245095d586d5920792f587f24df1e2
Alex Szabo <alex.szabo@elastic.co>
2025-07-22 15:59:47 +0200

    add more log appenders and limit FTR to a single config

--- x-pack/test/functional/apps/visualize/config.ts
+++ x-pack/test/functional/apps/visualize/config.ts
@@ -10,8 +10,35 @@ import { FtrConfigProviderContext } from '@kbn/test';
 export default async function ({ readConfigFile }: FtrConfigProviderContext) {
   const functionalConfig = await readConfigFile(require.resolve('../../config.base.js'));
 
+  const conf = functionalConfig.getAll();
+
+  conf.kbnTestServer.serverArgs.push(
+    `--logging.appenders.default=${JSON.stringify({
+      type: 'console',
+      layout: {
+        type: 'pattern',
+        pattern: '[%date][%level][%logger] %message %meta',
+      },
+    })}`,
+    `--logging.appenders.console=${JSON.stringify({
+      type: 'console',
+      layout: {
+        type: 'pattern',
+        pattern: '[%date][%level][%logger] %message %meta',
+      },
+    })}`,
+    `--logging.loggers=${JSON.stringify([
+      {
+        name: 'trace',
+        level: 'all',
+        appenders: ['console'],
+      },
+    ])}`,
+    `--logging.loggers.level=TRACE`
+  );
+
   return {
-    ...functionalConfig.getAll(),
+    ...conf,
     testFiles: [require.resolve('.')],
   };
 }
